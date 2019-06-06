61713cea9c0ee3c758126fa3998ac217d4a800dc
wzheng <hb_wangzheng@163.com>
2019-06-06 12:03:33 +0800

    add arithmetic into the project

---
+++ Arithmetic/README.md
@@ -0,0 +1,15 @@
+This is the notes collection for [arithmetic]()
+
+## How to use it
+
+Copy the repo into your disk first
+
+```bash
+$ git clone git@github.com:Wayley/front-end-notes.git
+```
+
+Then get into the [arithmetic folder](https://github.com/Wayley/front-end-notes/tree/master/arithmetic)
+
+```bash
+$ cd arithmetic/
+```
---
+++ Arithmetic/index.js
@@ -0,0 +1,29 @@
+function Arithmetic() {
+  this.version = '0.0.1';
+}
+// 记忆函数
+Arithmetic.prototype.memoizeFunction = function(fn) {
+  var cache = {};
+  return function() {
+    var key = arguments[0];
+    if (cache[key]) {
+      return cache[key];
+    } else {
+      var value = fn.apply(this, arguments);
+      cache[key] = value;
+      return value;
+    }
+  };
+};
+// 普通的斐波拉契
+Arithmetic.prototype.fibonacci = function(n) {
+  return n === 0 || n === 1 ? n : this.fibonacci(n - 1) + this.fibonacci(n - 2);
+};
+// 使用记忆函数优化后的斐波拉契
+Arithmetic.prototype.fibonacciWithMemoize = function() {
+  return this.memoizeFunction(function(n) {
+    return n === 0 || n === 1
+      ? n
+      : this.fibonacciWithMemoize(n - 1) + this.fibonacciWithMemoize(n - 2);
+  });
+}.call(new Arithmetic());
