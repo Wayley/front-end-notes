4e5d9ccc44f308e51095b27a7d8638aa466ba730
wayley <hb_wangzheng@163.com>
2019-08-27 18:16:44 +0800

    add some fs module function

--- NodeJS/fs/index.js
+++ NodeJS/fs/index.js
@@ -27,3 +27,58 @@ fs.readFile(path.join(__dirname, './README.md'), 'utf8', (err, data) => {
 });
 fs.copyFile('./README.md', function () {})
 console.log(process.cwd());
+
+
+
+function mkdir(path) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(path, {
+      recursive: true
+    }, err => {
+      if (err) {
+        reject(err)
+        throw err
+      }
+      resolve(path)
+    });
+  })
+}
+
+function readFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(err)
+        throw err
+      }
+      resolve(data)
+    })
+  })
+}
+
+function writeFile(file, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, 'utf8', err => {
+      if (err) {
+        reject(err)
+        throw err
+      }
+      resolve(data)
+    })
+  })
+
+}
+
+function copyTask(readFilePath, writeFilePath, Reg, replaceMent) {
+  return new Promise((resolve, reject) => {
+    readFile(readFilePath).then(data => {
+      let newData = (Reg && replaceMent) ? data.replace(Reg, replaceMent) : data;
+      return writeFile(writeFilePath, newData)
+    }).then(data => {
+      resolve(data)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+
+}
