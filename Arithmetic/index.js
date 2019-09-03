bb858d719381558684870f36b7f405de1b9e4c07
wayley <hb_wangzheng@163.com>
2019-09-03 11:28:12 +0800

    lint

--- Arithmetic/index.js
+++ Arithmetic/index.js
@@ -2,9 +2,9 @@ function Arithmetic() {
   this.version = '0.0.1';
 }
 // 记忆函数
-Arithmetic.prototype.memoizeFunction = function(fn) {
+Arithmetic.prototype.memoizeFunction = function (fn) {
   var cache = {};
-  return function() {
+  return function () {
     var key = arguments[0];
     if (cache[key]) {
       return cache[key];
@@ -16,14 +16,14 @@ Arithmetic.prototype.memoizeFunction = function(fn) {
   };
 };
 // 普通的斐波拉契
-Arithmetic.prototype.fibonacci = function(n) {
+Arithmetic.prototype.fibonacci = function (n) {
   return n === 0 || n === 1 ? n : this.fibonacci(n - 1) + this.fibonacci(n - 2);
 };
 // 使用记忆函数优化后的斐波拉契
-Arithmetic.prototype.fibonacciWithMemoize = function() {
-  return this.memoizeFunction(function(n) {
-    return n === 0 || n === 1
-      ? n
-      : this.fibonacciWithMemoize(n - 1) + this.fibonacciWithMemoize(n - 2);
+Arithmetic.prototype.fibonacciWithMemoize = function () {
+  return this.memoizeFunction(function (n) {
+    return n === 0 || n === 1 ?
+      n :
+      this.fibonacciWithMemoize(n - 1) + this.fibonacciWithMemoize(n - 2);
   });
 }.call(new Arithmetic());
